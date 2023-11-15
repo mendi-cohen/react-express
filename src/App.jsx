3759d35631bbc84a80b6552ebf27699e95475899
Your GitHub Username <your-email@example.com>
2023-11-15 23:56:54 +0200

    קצת CSS ועיצוב פשוט לטבלה

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,9 @@ function App() {
   const [products, setProducts] = useState([]); 
 
   async function fetchBooks() {
-    try {
       const response = await fetch("http://localhost:3001/Products/");
       const productsData = await response.json();
       setProducts(productsData);
-    } catch (error) {
-      console.error("Error fetching books:", error);
-    }
   }
 
   return (
@@ -21,18 +17,27 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <button onClick={fetchBooks}>
-        Get the Books!
+        Get the Products!
       </button>
 
       {/* אם אני מבקש את המערך כולו  */}
       <div>
-        {products.map((product, index) => (
 
-          <div key={index}>
-            <h1>  id: {product.id} price: {product.price}</h1>
+      <div className='flex'>
+
+      
+
+        {products.map((product, index) => (
+          <div key={index} className='grid'>
+            
+            <h1 className='grid-son-1'>  id:  </h1> <h2 className='grid-son-2'>{product.id}</h2>  
+            <h1 className='grid-son-1'> title: </h1>    <h2 className='grid-son-2'>{product.title} </h2> 
+            <h1 className='grid-son-1'> price: </h1> <h2 className='grid-son-2'>{product.price} </h2>
+            
           </div>
         ))}
          
+      </div>
       </div>
 
       {/* אם אני לוקח רק אחד /*}
